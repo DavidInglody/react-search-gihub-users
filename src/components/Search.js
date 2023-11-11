3ee02dc7f27fbad86564096d98b98dc3ce54adcc
David Inglody <inglody96@gmail.com>
2023-11-11 16:56:02 +0100

    initial commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user,setUser] = useState("")
+  const {requests,error, searchGithubUser,isLoading} = useContext(GithubContext)
+
+// get things from Global context
+  const handleSubmit= (e)=>{
+    e.preventDefault()
+    if(user){
+      searchGithubUser(user)
+    }
+  }
+
+  return <section className='section'>
+    <Wrapper className='section-center'>
+      {error.show && 
+      <ErrorWrapper>
+        <p>{error.msg}</p>
+      </ErrorWrapper>
+      }
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <MdSearch/>
+          <input type="text" value={user} onChange={(e)=>setUser(e.target.value)} placeholder='enter github user' />
+          {requests> 0 && !isLoading && <button type='submit'>search</button>}
+        </div>
+      </form>
+      <h3>request: {requests} / 60</h3>
+    </Wrapper>
+  </section>
 };
 
 const Wrapper = styled.div`
--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
+
+// 
+// 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain='dev-sdojv2530uo5abh7.us.auth0.com'
+      clientId= "JIBUdzzsmgwP8K3Ltcx7KEaDVbRBiwHN"
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+      >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
